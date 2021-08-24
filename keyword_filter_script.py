330cf2e892cef562344a0c86b9d0dd7ca4138bfe
neildesai <n7desai@gmail.com>
2021-08-24 14:55:18 +0100

    Update keyword_filter_script.py

--- keyword_filter_script.py
+++ keyword_filter_script.py
@@ -3,7 +3,10 @@ import re
 import numpy as np
 
 
-df = pd.read_excel('/Users/neil.desai/Downloads/onlinegambling.ca all keywords.xlsx')
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.write(df)
 
 df['Keyword'] = df['Keyword'].astype(str)
 
